407e0882e209f914a2128ba9c4dd24f3bd918a6a
LaupWing <loc_strict@hotmail.com>
2022-01-11 19:33:37 +0100

    update

--- gatsby-wp/gatsby-node.js
+++ gatsby-wp/gatsby-node.js
@@ -1,3 +1,36 @@
+const path = require('path')
+
+exports.createPages = async ({graphql, actions, reporter}) =>{
+   const { createPage } = actions
+
+   const result = await graphql(`
+      {
+         wp {
+         readingSettings {
+            postsPerPage
+         }
+         }
+         allWpCategory {
+         edges {
+            node {
+               id
+               name
+               count
+               uri
+               slug
+            }
+         }
+         }
+      }
+   `)
+   console.log(result)
+
+   if(result.errors){
+      reporter.panicOnBuild(`Something went wrong`, result.errors)
+   }
+   const {wp, allWpCategory} = result.data
+}
+
 // exports.createPages = async ({ actions }) => {
 //   const { createPage } = actions
 //   createPage({
