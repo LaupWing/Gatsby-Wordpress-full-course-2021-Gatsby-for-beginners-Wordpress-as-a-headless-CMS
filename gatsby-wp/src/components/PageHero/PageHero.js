b4279128928c4dfc56f9df784bad4c5bb2b32bf2
LaupWing <loc_strict@hotmail.com>
2022-01-11 17:37:50 +0100

    update

---
+++ gatsby-wp/src/components/PageHero/PageHero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyledImg } from './PageHero.styles'
+
+const PageHero = ({img}) => {
+   return (
+      <StyledImg image={img} alt="Page Hero">
+         
+      </StyledImg>
+   )
+}
+
+export default PageHero
