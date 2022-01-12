a822af7e5aec1d1c36b8e54dd388165bcdc4d13a
LaupWing <loc_strict@hotmail.com>
2022-01-12 15:14:59 +0100

    update

--- gatsby-wp/src/templates/archive.js
+++ gatsby-wp/src/templates/archive.js
@@ -1,9 +1,9 @@
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 import ArchiveSidebar from '../components/ArchiveSidebar/ArchiveSidebar'
 import BreadCrumb from '../components/BreadCrumb/BreadCrumb'
-import { ContentWrapper, Wrapper } from './archive.styles'
+import { ContentWrapper, PageContent, StyledDate, StyledH2, StyledReadMore, Wrapper } from './archive.styles'
 
 const archive = ({data: {allWpPost}, pageContext: { catId, catName, catUri, numPages, currentPages}}) => {
    return (
@@ -24,6 +24,30 @@ const archive = ({data: {allWpPost}, pageContext: { catId, catName, catUri, numP
             />
             <ContentWrapper>
                <ArchiveSidebar catId={catId} categories={categories.edges}/>
+               <PageContent>
+                  <h1 dangerouslySetInnerHTML={{__html: catName}}/>
+                  {allWpPost.edges.map(post => (
+                     <article key={post.node.id} className='entry-content'>
+                        <Link to={`/blog${post.node.uri}`}>
+                           <StyledH2
+                              dangerouslySetInnerHTML={{__html: post.node.title}}
+                           />
+                        </Link>
+                        <StyledDate
+                           dangerouslySetInnerHTML={{__html: post.node.data}}
+                        />
+                        <p
+                           dangerouslySetInnerHTML={{__html: post.node.excerpt}}
+                        />
+                        <StyledReadMore to={`/blog${post.node.uri}`}>
+                           Read More
+                        </StyledReadMore>
+                        <div className="dot-divider">
+                           
+                        </div>
+                     </article>
+                  ))}
+               </PageContent>
             </ContentWrapper>
          </Wrapper>
       </Layout>
