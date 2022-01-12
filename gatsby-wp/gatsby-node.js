a6a04067cbe46820b5bfa714475c2ea6983c7916
LaupWing <loc_strict@hotmail.com>
2022-01-12 14:14:06 +0100

    update

--- gatsby-wp/gatsby-node.js
+++ gatsby-wp/gatsby-node.js
@@ -3,6 +3,8 @@ const path = require('path')
 exports.createPages = async ({graphql, actions, reporter}) =>{
    const { createPage } = actions
 
+   const archive = path.resolve('./src/templates/archive.js')
+
    const result = await graphql(`
       {
          wp {
@@ -29,6 +31,32 @@ exports.createPages = async ({graphql, actions, reporter}) =>{
       reporter.panicOnBuild(`Something went wrong`, result.errors)
    }
    const {wp, allWpCategory} = result.data
+
+   allWpCategory.edges.forEach(category =>{
+      const postPerPage = wp.readingSettings.postsPerPage
+      const numberOfPosts = category.node.count
+      const numPages = Math.ceil(numberOfPosts / postPerPage)
+
+      if(numberOfPosts > 0 || category.node.name !== 'uncategorized'){
+         Array.from({length: numPages}).forEach((_, i)=>{
+            createPage({
+               path:
+                  i === 0 ? category.node.uri : `${category.node.uri}${i+1}`,
+               component: archive,
+               context:{
+                  limit: postsPerPage,
+                  skip: i * postsPerPage,
+                  numPages,
+                  currentPage: i + 1,
+                  catId: category.node.id,
+                  catName: category.node.name,
+                  catUri: category.node.uri,
+                  categories: allWpCategory
+               }
+            })
+         })
+      }
+   })
 }
 
 // exports.createPages = async ({ actions }) => {
